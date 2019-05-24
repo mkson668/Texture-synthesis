908928004ad8bbdbe14e48d889e476084ed8eea1
Aaron Wong <aaronwong668@gmail.com>
2019-05-23 22:14:37 -0700

    completed

---
+++ Holefill.py
@@ -0,0 +1,234 @@
+from PIL import Image, ImageDraw
+import numpy as np
+import random
+import os.path
+import pickle
+
+def ComputeSSD(TODOPatch, TODOMask, textureIm, patchL):
+	patch_rows, patch_cols, patch_bands = np.shape(TODOPatch)
+	tex_rows, tex_cols, tex_bands = np.shape(textureIm)
+	ssd_rows = tex_rows - 2 * patchL
+	ssd_cols = tex_cols - 2 * patchL
+	SSD = np.zeros((ssd_rows,ssd_cols))
+	# Get all the indices where TODOMask is equal to 0
+	indicesArray = np.argwhere(TODOMask == 0)
+	for r in range(ssd_rows):
+		for c in range(ssd_cols):
+			red, green, blue = 0, 0, 0
+			for x in indicesArray:
+				# Sum up all the SSD's for each color channel for each pixel at [r,c]
+				red += (textureIm[r+x[0], c+x[1], 0]*1.0 - TODOPatch[x[0],x[1],0])**2
+				green += (textureIm[r+x[0], c+x[1], 1]*1.0 - TODOPatch[x[0],x[1],1])**2
+				blue += (textureIm[r+x[0], c+x[1], 2]*1.0 - TODOPatch[x[0],x[1],2])**2
+			# Store the sum of SSD's for all chan to SSD matrix
+			SSD[r,c] = red + green + blue		
+	return SSD
+
+def CopyPatch(imHole,TODOMask,textureIm,iPatchCenter,jPatchCenter,iMatchCenter,jMatchCenter,patchL):
+	patchSize = 2 * patchL + 1
+	# indicesToBeFilled = np.argwhere(TODOMask == 1)
+	for i in range(patchSize):
+		for j in range(patchSize):
+			# Copy the selected patch selectPatch into the image containing
+			# the hole imHole for each pixel where TODOMask = 1.
+			# The patch is centred on iPatchCenter, jPatchCenter in the image imHole
+			#
+			# ADD YOUR CODE HERE
+			#
+			shift_i = i
+			shift_j = j
+			if TODOMask[i,j] == 1:
+				if i != patchL:
+					shift_i = i - patchL 
+				else:
+					shift_i = 0
+				if j != patchL:
+					shift_j = j - patchL
+				else:
+					shift_j = 0
+				imHole[iPatchCenter+shift_i, jPatchCenter+shift_j] = textureIm[iMatchCenter+shift_i, jMatchCenter+shift_j]
+	# temp = Image.fromarray(imHole).convert('RGB')
+	# temp.show()
+	return imHole
+
+##############################################################################
+#                            Some helper functions                           #
+##############################################################################
+
+def DrawBox(im,x1,y1,x2,y2):
+	draw = ImageDraw.Draw(im)
+	draw.line((x1,y1,x1,y2),fill="white",width=1)
+	draw.line((x1,y1,x2,y1),fill="white",width=1)
+	draw.line((x2,y2,x1,y2),fill="white",width=1)
+	draw.line((x2,y2,x2,y1),fill="white",width=1)
+	del draw
+	return im
+
+def Find_Edge(hole_mask):
+	[cols, rows] = np.shape(hole_mask)
+	edge_mask = np.zeros(np.shape(hole_mask))
+	for y in range(rows):
+		for x in range(cols):
+			if (hole_mask[x,y] == 1):
+				if (hole_mask[x-1,y] == 0 or
+						hole_mask[x+1,y] == 0 or
+						hole_mask[x,y-1] == 0 or
+						hole_mask[x,y+1] == 0):
+					edge_mask[x,y] = 1
+	return edge_mask
+
+##############################################################################
+#                           Main script starts here                          #
+##############################################################################
+
+#
+# Constants
+#
+
+# Change patchL to change the patch size used (patch size is 2 *patchL + 1)
+patchL = 20
+patchSize = 2*patchL+1
+
+# Standard deviation for random patch selection
+randomPatchSD = 6
+
+# Display results interactively
+showResults = True
+
+#
+# Read input image
+#
+
+im = Image.open('donkey.jpg').convert('RGB')
+# im = Image.open('plane.jpg').convert('RGB')
+# im = Image.open('paul.jpg').convert('RGB')
+im_array = np.asarray(im, dtype=np.uint8)
+imRows, imCols, imBands = np.shape(im_array)
+
+#
+# Define hole and texture regions.  This will use files fill_region.pkl and
+#   texture_region.pkl, if both exist, otherwise user has to select the regions.
+if os.path.isfile('fill_region.pkl') and os.path.isfile('texture_region.pkl'):
+	fill_region_file = open('fill_region.pkl', 'rb')
+	fillRegion = pickle.load( fill_region_file )
+	fill_region_file.close()
+
+	texture_region_file = open('texture_region.pkl', 'rb')
+	textureRegion = pickle.load( texture_region_file )
+	texture_region_file.close()
+else:
+	# ask the user to define the regions
+	print ("Specify the fill and texture regions using polyselect.py")
+	exit()
+
+#
+# Get coordinates for hole and texture regions
+#
+
+fill_indices = fillRegion.nonzero()
+nFill = len(fill_indices[0])                # number of pixels to be filled
+iFillMax = max(fill_indices[0])
+iFillMin = min(fill_indices[0])
+jFillMax = max(fill_indices[1])
+jFillMin = min(fill_indices[1])
+assert((iFillMin >= patchL) and
+		(iFillMax < imRows - patchL) and
+		(jFillMin >= patchL) and
+		(jFillMax < imCols - patchL)) , "Hole is too close to edge of image for this patch size"
+
+texture_indices = textureRegion.nonzero()
+iTextureMax = max(texture_indices[0])
+iTextureMin = min(texture_indices[0])
+jTextureMax = max(texture_indices[1])
+jTextureMin = min(texture_indices[1])
+textureIm   = im_array[iTextureMin:iTextureMax+1, jTextureMin:jTextureMax+1, :]
+texImRows, texImCols, texImBands = np.shape(textureIm)
+assert((texImRows > patchSize) and
+		(texImCols > patchSize)) , "Texture image is smaller than patch size"
+
+#
+# Initialize imHole for texture synthesis (i.e., set fill pixels to 0)
+#
+
+imHole = im_array.copy()
+imHole[fill_indices] = 0
+
+#
+# Is the user happy with fillRegion and textureIm?
+#
+if showResults == True:
+	# original
+	im.show()
+	# convert to a PIL image, show fillRegion and draw a box around textureIm
+	im1 = Image.fromarray(imHole).convert('RGB')
+	im1 = DrawBox(im1,jTextureMin,iTextureMin,jTextureMax,iTextureMax)
+	im1.show()
+	print ("Are you happy with this choice of fillRegion and textureIm?")
+	Yes_or_No = False
+	while not Yes_or_No:
+		answer = input("Yes or No: ")
+		if answer == "Yes" or answer == "No":
+			Yes_or_No = True
+	assert answer == "Yes", "You must be happy. Please try again."
+
+#
+# Perform the hole filling
+#
+
+while (nFill > 0):
+	print ("Number of pixels remaining = {}".format(nFill))
+
+	# Set TODORegion to pixels on the boundary of the current fillRegion
+	TODORegion = Find_Edge(fillRegion)
+	edge_pixels = TODORegion.nonzero()
+	nTODO = len(edge_pixels[0])
+
+	while(nTODO > 0):
+
+		# Pick a random pixel from the TODORegion
+		index = np.random.randint(0,nTODO)
+		iPatchCenter = edge_pixels[0][index]
+		jPatchCenter = edge_pixels[1][index]
+
+		# Define the coordinates for the TODOPatch
+		TODOPatch = imHole[iPatchCenter-patchL:iPatchCenter+patchL+1,jPatchCenter-patchL:jPatchCenter+patchL+1,:]
+		TODOMask = fillRegion[iPatchCenter-patchL:iPatchCenter+patchL+1,jPatchCenter-patchL:jPatchCenter+patchL+1]
+
+		#
+		# Compute masked SSD of TODOPatch and textureIm
+		#
+		ssdIm = ComputeSSD(TODOPatch, TODOMask, textureIm, patchL)
+
+		# Randomized selection of one of the best texture patches
+		ssdIm1 = np.sort(np.copy(ssdIm),axis=None)
+		ssdValue = ssdIm1[int(min(round(abs(random.gauss(0,randomPatchSD))),np.size(ssdIm1)-1))]
+		ssdIndex = np.nonzero(ssdIm==ssdValue)
+		iSelectCenter = ssdIndex[0][0]
+		jSelectCenter = ssdIndex[1][0]
+
+		# adjust i, j coordinates relative to textureIm
+		iSelectCenter = iSelectCenter + patchL
+		jSelectCenter = jSelectCenter + patchL
+		selectPatch = textureIm[iSelectCenter-patchL:iSelectCenter+patchL+1,jSelectCenter-patchL:jSelectCenter+patchL+1,:]
+
+		#
+		# Copy patch into hole
+		#
+		imHole = CopyPatch(imHole,TODOMask,textureIm,iPatchCenter,jPatchCenter,iSelectCenter,jSelectCenter,patchL)
+
+		# Update TODORegion and fillRegion by removing locations that overlapped the patch
+		TODORegion[iPatchCenter-patchL:iPatchCenter+patchL+1,jPatchCenter-patchL:jPatchCenter+patchL+1] = 0
+		fillRegion[iPatchCenter-patchL:iPatchCenter+patchL+1,jPatchCenter-patchL:jPatchCenter+patchL+1] = 0
+
+		edge_pixels = TODORegion.nonzero()
+		nTODO = len(edge_pixels[0])
+
+	fill_indices = fillRegion.nonzero()
+	nFill = len(fill_indices[0])
+
+#
+# Output results
+#
+if showResults == True:
+	Image.fromarray(imHole).convert('RGB').show()
+Image.fromarray(imHole).convert('RGB').save('results.jpg')
